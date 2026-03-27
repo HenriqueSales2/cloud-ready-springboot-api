3f745e42e86590e08049cd496579ea8bcdb1ee6f
Henrique Sales <uchihaitachi19112007@gmail.com>
2026-03-27 17:12:17 -0300

    adding Migrations with Flyway

---
+++ 08_MigrationsWithFlyway/src/main/java/br/com/application/controllers/person/PersonController.java
@@ -0,0 +1,60 @@
+package br.com.application.controllers.person;
+
+import br.com.application.data.dto.PersonDTO;
+import br.com.application.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService service; // com a dependência Service
+    // private PersonService service =  new PersonService(); sem a dependência Service
+
+
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO create(@RequestBody PersonDTO personDTO) {
+        return service.create(personDTO);
+    }
+
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO update(@RequestBody PersonDTO personDTO) {
+        return service.update(personDTO);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ 08_MigrationsWithFlyway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+-- Copiando dados para a tabela rest_with_spring_boot_java.person: ~4 rows (aproximadamente)
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1, 'São Paulo - Brasil', 'First', 'Female', 'User'),
+(2, 'Minas Gerais - Brasil', 'Second', 'Male', 'User'),
+(3, 'Rio de Janeiro - Brasil', 'Third', 'Female', 'User'),
+(8, 'New York - United States', 'Some', 'Male', 'User');
+
